7a41970319aaee202a79d0dea7388e1382f32eb6
DanielMi113r <93039964+DanielMi113r@users.noreply.github.com>
2021-10-23 16:08:25 +0300

    Add files via upload
    Решение задачи

---
+++ zamena_sl.py
@@ -0,0 +1,14 @@
+from collections import Counter
+def zamena_sl(word) :
+	word = word.lower()
+	word = list(word)
+	count = Counter(word)
+
+		result = ""
+	
+		for char in word:
+if count[char] == 1:
+	result += "("
+else:
+	result += ")"
+return result
